8ce26e5177069566a0c8554cfd90350d378233f3
Olga Trubetskaia <olga_trubetskaia@epam.com>
2022-02-10 11:01:39 +0000

    Embedded PostgreSQL (zonky) for unit tests

--- speciome-catalog-persistence-spring-data-jpa/src/test/java/com/epam/speciome/catalog/persistence/spring/SpringUserStorageTest.java
+++ speciome-catalog-persistence-spring-data-jpa/src/test/java/com/epam/speciome/catalog/persistence/spring/SpringUserStorageTest.java
@@ -4,15 +4,15 @@ import com.epam.speciome.catalog.persistence.api.users.UserStorage;
 import com.epam.speciome.catalog.persistence.api.users.UserStorageContract;
 import com.epam.speciome.catalog.persistence.spring.users.SpringUserStorage;
 import com.epam.speciome.catalog.persistence.spring.users.UserJpaRepository;
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureEmbeddedDatabase(beanName = "DataSource", provider = AutoConfigureEmbeddedDatabase.DatabaseProvider.ZONKY)
 @DisplayName("Test Spring Data JPA user storage implementation")
 public class SpringUserStorageTest implements UserStorageContract {
     private UserStorage userStorage;
