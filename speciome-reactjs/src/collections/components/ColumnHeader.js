7f2fa711db015f11e1cc5e594905ea4d7da2f356
Dmitrii Adamian <Dmitrii_Adamian@epam.com>
2022-03-31 12:23:18 +0300

    ColumnHeader WIP - callbacks added

--- speciome-reactjs/src/collections/components/ColumnHeader.js
+++ speciome-reactjs/src/collections/components/ColumnHeader.js
@@ -1,19 +1,73 @@
-import { SortDown, SortUp } from 'react-bootstrap-icons';
+import {SortDown, SortUp} from 'react-bootstrap-icons';
+
+const ascendingTitle = 'ascending';
+const descendingTitle = 'descending';
+
+const selectedColor = 'black';
+const notSelectedColor = 'grey'
+
+function AscendingIconNotSelected(props) {
+    const {onSortAscending} = props;
+    return <SortUp title={ascendingTitle} color={notSelectedColor} onClick={onSortAscending}/>;
+}
+
+function AscendingIconSelected(props) {
+    const {onClearSort} = props;
+    return <SortUp title={ascendingTitle} color={selectedColor} onClick={onClearSort}/>;
+}
+
+function AscendingIcon(props) {
+    const {selected, onSortAscending, onClearSort} = props;
+    return selected ?
+        <AscendingIconSelected onClearSort={onClearSort}/> :
+        <AscendingIconNotSelected onSortAscending={onSortAscending}/>;
+}
+
+function DescendingIconNotSelected(props) {
+    const {onSortDescending} = props;
+    return <SortDown title={descendingTitle} color={notSelectedColor} onClick={onSortDescending}/>;
+}
+
+function DescendingIconSelected(props) {
+    const {onClearSort} = props;
+    return <SortDown title={descendingTitle} color={selectedColor} onClick={onClearSort}/>;
+}
+
+function DescendingIcon(props) {
+    const {selected, onSortDescending, onClearSort} = props;
+    return selected ?
+        <DescendingIconSelected onClearSort={onClearSort}/> :
+        <DescendingIconNotSelected onSortDescending={onSortDescending}/>;
+}
 
 function ColumnHeader(props) {
-    const {name, sortBy, sortDirection} = props;
+    const {name, sortBy, sortDirection, onSortAscending, onSortDescending, onClearSort} = props;
 
     const sameColumn = name === sortBy;
     const showAscending = sameColumn ? sortDirection === 'ascending' : true;
-    const ascendingColor = (sameColumn && sortDirection === 'ascending') ? 'black' : 'grey';
+    const ascendingSelected = sameColumn && sortDirection === 'ascending';
 
     const showDescending = sameColumn ? sortDirection === 'descending' : true;
-    const descendingColor = (sameColumn && sortDirection === 'descending') ? 'black' : 'grey';
+    const descendingSelected = sameColumn && sortDirection === 'descending';
     return (
         <div>
             {name}
-            {showAscending ? <SortUp title='ascending' color={ascendingColor}/> : <div/>}
-            {showDescending ? <SortUp title='descending' color={descendingColor}/> : <div/>}
+            {showAscending ?
+                <AscendingIcon
+                    selected={ascendingSelected}
+                    onSortAscending={onSortAscending}
+                    onClearSort={onClearSort}
+                />
+                : <div/>
+            }
+            {showDescending ?
+                <DescendingIcon
+                    selected={descendingSelected}
+                    onSortDescending={onSortDescending}
+                    onClearSort={onClearSort}
+                />
+                : <div/>
+            }
         </div>
     );
 }
--- speciome-reactjs/src/collections/components/ColumnHeader.test.js
+++ speciome-reactjs/src/collections/components/ColumnHeader.test.js
@@ -1,8 +1,14 @@
-import {render, screen} from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
 
 import ColumnHeader from './ColumnHeader.js';
 
 const columnName = 'column name';
+
+function findIcons() {
+    const ascendingIcon = screen.queryByTitle('ascending', {exact: true});
+    const descendingIcon = screen.queryByTitle('descending', {exact: true});
+    return {ascendingIcon, descendingIcon};
+}
 
 describe('Rendering tests without sorting', () => {
     beforeEach(() => render(<ColumnHeader name={columnName}/>));
@@ -22,8 +28,7 @@ describe('Rendering tests with sorting', () => {
     test('Same column - one icon displayed in black', () => {
         render(<ColumnHeader name={columnName} sortBy={columnName} sortDirection={'ascending'}/>);
 
-        const ascendingIcon = screen.queryByTitle('ascending');
-        const descendingIcon = screen.queryByTitle('descending');
+        const {ascendingIcon, descendingIcon} = findIcons();
 
         expect(descendingIcon).toBeNull();
         expect(ascendingIcon).not.toBeNull();
@@ -34,12 +39,76 @@ describe('Rendering tests with sorting', () => {
         const otherColumn = 'other column';
         render(<ColumnHeader name={columnName} sortBy={otherColumn} sortDirection={'ascending'}/>);
 
-        const ascendingIcon = screen.queryByTitle('ascending');
-        const descendingIcon = screen.queryByTitle('descending');
+        const {ascendingIcon, descendingIcon} = findIcons();
 
         expect(descendingIcon).not.toBeNull();
         expect(descendingIcon.getAttribute('fill')).toBe('grey');
         expect(ascendingIcon).not.toBeNull();
         expect(ascendingIcon.getAttribute('fill')).toBe('grey');
     })
+});
+
+describe('Callback tests', () => {
+    describe('No sorting selected', () => {
+        const setupMocks = () => {
+            const onSortAscending = jest.fn();
+            const onSortDescending = jest.fn();
+
+            render(<ColumnHeader
+                name={columnName}
+                onSortAscending={onSortAscending}
+                onSortDescending={onSortDescending}/>
+            );
+            return {onSortAscending, onSortDescending};
+        }
+
+        test('Ascending icon clicked - onSortAscending called', () => {
+            const {onSortAscending, onSortDescending} = setupMocks();
+            const {ascendingIcon} = findIcons();
+
+            fireEvent.click(ascendingIcon);
+            expect(onSortAscending).toHaveBeenCalledTimes(1);
+            expect(onSortDescending).toHaveBeenCalledTimes(0);
+        });
+
+        test('Descending icon clicked - onSortDescending called', () => {
+            const {onSortAscending, onSortDescending} = setupMocks();
+            const {descendingIcon} = findIcons();
+
+            fireEvent.click(descendingIcon);
+            expect(onSortAscending).toHaveBeenCalledTimes(0);
+            expect(onSortDescending).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    describe('Sorting selected', () => {
+        const setupMocks = (sortDirection) => {
+            const onClearSort = jest.fn();
+
+            render(<ColumnHeader
+                name={columnName}
+                sortBy={columnName}
+                sortDirection={sortDirection}
+                onClearSort={onClearSort}/>
+            );
+            return {onClearSort};
+        }
+
+        test('Ascending icon clicked - onClearSort called', () => {
+            const {onClearSort} = setupMocks('ascending');
+            const {ascendingIcon} = findIcons();
+
+            fireEvent.click(ascendingIcon);
+            expect(onClearSort).toHaveBeenCalledTimes(1);
+        });
+
+        test('Descending icon clicked - onSortDescending called', () => {
+            const {onClearSort} = setupMocks('descending');
+            const {descendingIcon} = findIcons();
+
+            fireEvent.click(descendingIcon);
+            expect(onClearSort).toHaveBeenCalledTimes(1);
+        });
+    });
+    // Sorting selected - onClearSort
 });
